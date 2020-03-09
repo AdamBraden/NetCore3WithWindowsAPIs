8883559dd2d748a7b6192ac150ecacccde027bd5
Adam Braden <adambr@microsoft.com>
2020-03-09 16:36:53 -0700

    Add project files.

---
+++ NC3ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace NC3ConsoleApp
+{
+    class Program
+    {
+        public static string PackageId;
+        public static string PackageDirectory;
+        public static bool HasPackageIdentity { get; private set; }
+        public static string ExecutableDirectory;
+
+        static void Main(string[] args)
+        {
+
+            ExecutableDirectory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName).ToLowerInvariant();
+
+            //Packaging APIs
+            try
+            {
+                var package = Windows.ApplicationModel.Package.Current;
+                PackageId = package.Id.FullName;
+                PackageDirectory = package.InstalledLocation.Path.ToLowerInvariant();
+            }
+
+            // App is running without being packaged at all
+            catch (Exception ex) when (ex.HResult == -2147009196)
+            {
+                PackageId = "Not packaged";
+                PackageDirectory = "Not packaged";
+                HasPackageIdentity = false;
+            }
+            Console.WriteLine($"My name is {PackageId}");
+            Console.WriteLine($"My path is {PackageDirectory}");
+            Console.Write("Press key to continue");
+            Console.ReadLine();
+        }
+    }
+}
